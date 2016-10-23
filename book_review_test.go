6741fd967fe7b02a3142c2f10380fa85d8c54c79
Cedric Chin <cedric@elijames.org>
2016-10-23 17:42:09 +0800

    Start work on the book_review models.

--- book_review_test.go
+++ book_review_test.go
@@ -1,1 +1,30 @@
 package grepbook_test
+
+import (
+	"testing"
+
+	"github.com/ejamesc/grepbook"
+)
+
+func TestCreateBookReview(t *testing.T) {
+	chapters := grepbook.CreateChapter("Introduction, Preface")
+	br, err := testDB.CreateBookReview("Superintelligence", "Nick Bostrom", "<p>Hello</p>", "{}", chapters)
+	ok(t, err)
+	assert(t, br.UID != "", "expect uid to be filled with string")
+	assert(t, br.Title != "", "expect book review title to be filled with string")
+	assert(t, br.BookAuthor != "", "expect book author to be filled with string")
+	assert(t, br.HTML == "<p>Hello</p>", "expect HTML to be empty")
+	equals(t, 2, len(br.Chapters))
+}
+
+func TestGetBookReview(t *testing.T) {
+
+}
+
+func TestDeleteBookReview(t *testing.T) {
+
+}
+
+func TestBookReviewSave(t *testing.T) {
+
+}
