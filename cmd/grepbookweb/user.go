488fd69901a7365fec2d54002331385c51f29dda
Cedric Chin <cedric@elijames.org>
2016-12-29 09:01:09 +0700

    Hook up user profile page to Handler.

--- cmd/grepbookweb/user.go
+++ cmd/grepbookweb/user.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/asaskevich/govalidator"
 	"github.com/ejamesc/grepbook"
 )
 
@@ -31,8 +33,59 @@ func (a *App) UserProfileHandler() HandlerWithError {
 	}
 }
 
-func (a *App) UserEditHandler(db grepbook.BookReviewDB) HandlerWithError {
+func (a *App) UserEditHandler(db grepbook.UserDB) HandlerWithError {
 	return func(w http.ResponseWriter, req *http.Request) error {
+		reloginTarget := "/user"
+		email, name := req.FormValue("email"), req.FormValue("name")
+		oldPass, newPass, newPass2 := req.FormValue("old-password"), req.FormValue("new-password"), req.FormValue("new-password2")
+
+		email, name = strings.TrimSpace(email), strings.TrimSpace(name)
+		oldPass, newPass, newPass2 = strings.TrimSpace(oldPass), strings.TrimSpace(newPass), strings.TrimSpace(newPass2)
+
+		if !govalidator.IsEmail(email) {
+			a.saveFlash(w, req, "That's not a valid email address")
+			http.Redirect(w, req, reloginTarget, 302)
+			return newError(400, "Invalid email provided", nil)
+		}
+
+		userDelta := grepbook.UserDelta{Email: email}
+
+		if oldPass != "" || newPass != "" || newPass2 != "" {
+			if oldPass == "" {
+				a.saveFlash(w, req, "You need to provide your old password")
+				http.Redirect(w, req, reloginTarget, 302)
+				return newError(400, "No old password provided", nil)
+			}
+			if newPass != newPass2 {
+				a.saveFlash(w, req, "Your new passwords do not match!")
+				http.Redirect(w, req, reloginTarget, 302)
+				return newError(400, "New passwords do not match", nil)
+			}
+			if newPass == "" || newPass2 == "" {
+				a.saveFlash(w, req, "One of the new password slots was left empty")
+				http.Redirect(w, req, reloginTarget, 302)
+				return newError(400, "One of the new password slots was left empty", nil)
+			}
+		}
+
+		user := getUser(req)
+		if !db.IsUserPasswordCorrect(user.Email, oldPass) {
+			a.saveFlash(w, req, "Old password wrong")
+			http.Redirect(w, req, reloginTarget, 302)
+			return newError(400, "Old password wrong", nil)
+
+		}
+		userDelta.Password = newPass
+		userDelta.Name = name
+
+		_, err := db.UpdateUser(user.Email, userDelta)
+		if err != nil {
+			a.saveFlash(w, req, "An error occured when saving your data")
+			http.Redirect(w, req, reloginTarget, 302)
+			return err
+		}
+
+		http.Redirect(w, req, reloginTarget, 302)
 		return nil
 	}
 }
